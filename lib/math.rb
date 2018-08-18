036a8f4588946c1c4a16310b7fb765b812ae6cdc
Jackie Ha <jha2@wellesley.edu>
2018-08-18 14:22:28 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -19,9 +19,10 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-  Math.sqart
+  Math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
+  
   #Hint:  __ + (( __ * __ ) / __ )
 end
